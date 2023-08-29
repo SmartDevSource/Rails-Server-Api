a156b5eb70e03c863646e2891ad2577bb8fa8c8b
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2023-08-29 21:52:53 +0200

    Last push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,7 @@ User.destroy_all
 
 User.create(email: "manu", password: "boubou")
 User.create(email: "guigui", password: "boubou")
-User.create(email: "robert", password: "boubou")
-User.create(email: "laura", password: "boubou")
-User.create(email: "ginette", password: "boubou")
 
-for i in 0..20
+for i in 0..5
     Article.create(title: Faker::Book.title, content: Faker::Markdown.emphasis, author: User.all.sample.email);
 end
